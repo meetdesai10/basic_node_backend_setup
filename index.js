72c8f27218115138c539ad37f703b86844a96eee
meetdesai10 <dmeet1008@gmail.com>
2025-03-16 00:52:01 +0530

    :

--- index.js
+++ index.js
@@ -1,39 +1,80 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+const http = require("http");
+const { Server } = require("socket.io");
 const cookieParser = require("cookie-parser");
 const { DBCONNECT } = require("./src/db/db");
 const app = express();
+const server = http.createServer(app);
+// all about ws
+// create WS socket
+// strict origin cors configuration
+const allowedOrigins = ["http://localhost:9999"];
+// socket start io server
+const io = new Server(server);
+// apis cors configuration
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) {
+        return callback(new Error("Unauthorized"));
+      }
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+      return callback(new Error("Unauthorized"));
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "X-API-Key",
+    ],
+    credentials: true,
+    exposedHeaders: ["set-cookie"],
+  })
+);
+// send error when client is not authorized
+app.use((err, req, res, next) => {
+  if (err?.message == "Unauthorized") {
+    return res
+      .status(403)
+      .json({ code: 403, success: false, error: "Unauthorized request!!" });
+  }
+});
+// connection
+io.on("connection", (socket) => {
+  console.log("A new user has connected", socket.id);
+  // socket.on("user-message", (data) => {
+  //   io.emit("message", data);
+  // });
+});
 const PORT = process.env.PORT;
-
 // get router file
 const userRoutes = require("./src/routes/user.routes");
-
-// cors configuration
-app.use(cors());
 // set limit on data
 app.use(express.json({ limit: "500kb" }));
 // set cookie parser
 app.use(cookieParser());
 // uncode url
 app.use(express.urlencoded({ extended: true }));
+// allow file to render
+app.use(express.static(path.resolve("./public")));
+// routes entry point
 // testing route
 app.get("/", (req, res) => {
-  res.status(200).send({
-    status: "success",
-    code: 200,
-    data: { data: "Server is running..." },
-  });
+  res.sendFile("/public/index.html");
 });
-
-// routes entry point
+// all the routes redirection
 app.use("/api/v1/users", userRoutes);
-
 // DB connection
 DBCONNECT()
   .then((res) => {
     console.log("MongoDb conected.");
-    app.listen(PORT, (res) => {
+    server.listen(PORT, (res) => {
       console.log("Server is listining on port: ", PORT);
     });
   })
